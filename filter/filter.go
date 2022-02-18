e37af25024dbd8a5fab2940fd8bf29f44e77cf37
rwxrob <rob@rwx.gg>
2022-02-17 20:15:56 -0500

    Add filter.Println and fix filter.P bug

--- filter/filter.go
+++ filter/filter.go
@@ -16,7 +16,10 @@ limitations under the License.
 
 package filter
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Number combines the primitives generally considered numbers by JSON
 // and other high-level structure data representations.
@@ -33,18 +36,24 @@ type Text interface {
 // representations in JSON and other high-level structured data
 // representations.
 type P interface {
-	Number
-	Text
-	bool
+	int16 | int32 | int64 | float32 | float64 |
+		[]byte | string | bool
+}
+
+// Println prints ever element of the set.
+func Println[T P](set []T) {
+	for _, i := range set {
+		fmt.Println(i)
+	}
 }
 
 // HasPrefix filters the Text input set and returns only those elements
 // that have the give prefix.
 func HasPrefix[T Text](set []T, pre string) []T {
 	m := []T{}
-	for _, s := range set {
-		if strings.HasPrefix(string(s), pre) {
-			m = append(m, s)
+	for _, i := range set {
+		if strings.HasPrefix(string(i), pre) {
+			m = append(m, i)
 		}
 	}
 	return m
