dad2ffd579c3f4d0d9fb0bc537ff33b232bc4e01
rwxrob <rob@rwx.gg>
2022-04-01 08:41:41 -0400

    Move help into bonzai, remove inc, breaking change

--- inc/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Robert S. Muhlestein.
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-Package inc contains a predefined set of popular commands that some may
-choose to include/compose into their Bonzai command trees.
-*/
-package inc
