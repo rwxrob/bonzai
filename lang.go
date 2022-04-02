3072deb66bf95aebedc6b250db1c311021fbcc39
rishabhdeepsingh <Thegame@MAC15>
2022-04-02 17:51:03 +0530

    Refactor strings from bonzai.go

---
+++ lang.go
@@ -0,0 +1,8 @@
+package Z
+
+var (
+	CommandMissing                = "multicall command missing"
+	FirstValueMustBeCmd           = "first value must be *Cmd"
+	OnlyStringArgumentsAllowed    = "only string arguments allowed"
+	UnmappedMulticallCommandError = "unmapped multicall command: %v"
+)
