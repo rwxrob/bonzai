72f64a8244e7ce73b69aef2b2f6e5cfdfdb3ce1b
Rob Muhlestein <rob@rwx.gg>
2022-11-06 20:07:16 -0500

    Add separation and MissingVar to errors

--- z/errors.go
+++ z/errors.go
@@ -12,6 +12,8 @@ func (e NotEnoughArgs) Error() string {
 		"error: %v is not enough args, %v required", e.Count, e.Min)
 }
 
+// -------------------------------- -- --------------------------------
+
 type TooManyArgs struct {
 	Count int
 	Max   int
@@ -21,6 +23,8 @@ func (e TooManyArgs) Error() string {
 	return fmt.Sprintf(
 		"error: %v is too many args, %v maximum", e.Count, e.Max)
 }
+
+// -------------------------------- -- --------------------------------
 
 type WrongNumArgs struct {
 	Count int
@@ -32,6 +36,8 @@ func (e WrongNumArgs) Error() string {
 		"error: %v is wrong number of args, %v required", e.Count, e.Num)
 }
 
+// -------------------------------- -- --------------------------------
+
 type MissingConf struct {
 	Path string
 }
@@ -39,6 +45,18 @@ type MissingConf struct {
 func (e MissingConf) Error() string {
 	return fmt.Sprintf("missing conf value for %v", e.Path)
 }
+
+// -------------------------------- -- --------------------------------
+
+type MissingVar struct {
+	Path string
+}
+
+func (e MissingVar) Error() string {
+	return fmt.Sprintf("missing var for %v", e.Path)
+}
+
+// -------------------------------- -- --------------------------------
 
 type UsesConf struct {
 	Cmd *Cmd
@@ -48,6 +66,8 @@ func (e UsesConf) Error() string {
 	return fmt.Sprintf("%v requires Z.Conf", e.Cmd.Name)
 }
 
+// -------------------------------- -- --------------------------------
+
 type UsesVars struct {
 	Cmd *Cmd
 }
@@ -55,6 +75,8 @@ type UsesVars struct {
 func (e UsesVars) Error() string {
 	return fmt.Sprintf("%v requires Z.Vars", e.Cmd.Name)
 }
+
+// -------------------------------- -- --------------------------------
 
 type NoCallNoCommands struct {
 	Cmd *Cmd
@@ -64,6 +86,8 @@ func (e NoCallNoCommands) Error() string {
 	return fmt.Sprintf("%v requires either Call or Commands", e.Cmd.Name)
 }
 
+// -------------------------------- -- --------------------------------
+
 type DefCmdReqCall struct {
 	Cmd *Cmd
 }
@@ -71,6 +95,8 @@ type DefCmdReqCall struct {
 func (e DefCmdReqCall) Error() string {
 	return fmt.Sprintf("default (first) %q Commands requires Call", e.Cmd.Name)
 }
+
+// -------------------------------- -- --------------------------------
 
 type IncorrectUsage struct {
 	Cmd *Cmd
@@ -80,6 +106,8 @@ func (e IncorrectUsage) Error() string {
 	return fmt.Sprintf("usage: %v %v", e.Cmd.Name, e.Cmd.GetUsage())
 }
 
+// -------------------------------- -- --------------------------------
+
 type MultiCallCmdNotFound struct {
 	CmdName string
 }
@@ -87,6 +115,8 @@ type MultiCallCmdNotFound struct {
 func (e MultiCallCmdNotFound) Error() string {
 	return fmt.Sprintf("multicall command not found: %v", e.CmdName)
 }
+
+// -------------------------------- -- --------------------------------
 
 type MultiCallCmdNotCmd struct {
 	CmdName string
@@ -97,6 +127,8 @@ func (e MultiCallCmdNotCmd) Error() string {
 	return fmt.Sprintf(
 		"multicall match for %v, but first in slice not *Z.Cmd: %T", e.CmdName, e.It)
 }
+
+// -------------------------------- -- --------------------------------
 
 type MultiCallCmdArgNotString struct {
 	CmdName string
