01c2f3894746a860342b2686b35360b94b48cdd2
Rob Muhlestein <rob@rwx.gg>
2024-10-18 10:36:04 -0400

    add json package

---
+++ z/json/fetch_test.go
@@ -0,0 +1,127 @@
+package json_test
+
+import (
+	"fmt"
+	_http "net/http"
+	ht "net/http/httptest"
+
+	json "github.com/rwxrob/bonzai/z/json"
+)
+
+func ExampleFetch() {
+
+	// serve get
+	handler := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `{"get":"t"}`)
+		})
+	svr := ht.NewServer(handler)
+	defer svr.Close()
+
+	// serve get int
+	handler0 := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `20220322075441`)
+		})
+	svr0 := ht.NewServer(handler0)
+	defer svr0.Close()
+
+	// serve post
+	handler1 := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `{"post":"t","c":"t"}`)
+		})
+	svr1 := ht.NewServer(handler1)
+	defer svr1.Close()
+
+	// serve put
+	handler2 := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `{"put":"t"}`)
+		})
+	svr2 := ht.NewServer(handler2)
+	defer svr2.Close()
+
+	// serve patch
+	handler3 := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `{"patch":"t"}`)
+		})
+	svr3 := ht.NewServer(handler3)
+	defer svr3.Close()
+
+	// serve delete
+	handler4 := _http.HandlerFunc(
+		func(w _http.ResponseWriter, r *_http.Request) {
+			fmt.Fprintf(w, `{"delete":"t"}`)
+		})
+	svr4 := ht.NewServer(handler4)
+	defer svr4.Close()
+
+	json.TimeOut = 4
+
+	// create the struct type matching the REST query JSON
+	type Data struct {
+		Get     string `json:"get"`
+		Post    string `json:"post"`
+		Put     string `json:"put"`
+		Patch   string `json:"patch"`
+		Delete  string `json:"delete"`
+		Changed string `json:"c"`
+		Ignored string `json:"i"`
+	}
+
+	data := &Data{
+		Changed: "o",
+		Ignored: "i",
+	}
+	jsdata := json.This{data}
+	jsdata.Print()
+
+	req := &json.Request{URL: svr.URL, Into: data}
+
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	jsdata.Print()
+
+	anint := 0
+	req = &json.Request{URL: svr0.URL, Into: &anint}
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(anint)
+
+	req = &json.Request{Method: `POST`, URL: svr1.URL, Into: data}
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	jsdata.Print()
+
+	req = &json.Request{Method: `PUT`, URL: svr2.URL, Into: data}
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	jsdata.Print()
+
+	req = &json.Request{Method: `PATCH`, URL: svr3.URL, Into: data}
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	jsdata.Print()
+
+	req = &json.Request{Method: `DELETE`, URL: svr4.URL, Into: data}
+	if err := json.Fetch(req); err != nil {
+		fmt.Println(err)
+	}
+	jsdata.Print()
+
+	// Output:
+	// {"get":"","post":"","put":"","patch":"","delete":"","c":"o","i":"i"}
+	// {"get":"t","post":"","put":"","patch":"","delete":"","c":"o","i":"i"}
+	// 20220322075441
+	// {"get":"t","post":"t","put":"","patch":"","delete":"","c":"t","i":"i"}
+	// {"get":"t","post":"t","put":"t","patch":"","delete":"","c":"t","i":"i"}
+	// {"get":"t","post":"t","put":"t","patch":"t","delete":"","c":"t","i":"i"}
+	// {"get":"t","post":"t","put":"t","patch":"t","delete":"t","c":"t","i":"i"}
+}
---
+++ z/json/json.go
@@ -0,0 +1,107 @@
+/*
+Package json contains interface specifications for representing any Go
+type as JSON where possible. Using the goprintasjson tool allows for quick code generation of scaffolding to make any Go type easily used as JSON.
+*/
+package json
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+)
+
+// specification (unlike the encoding/json standard which defaults to
+// escaping many other characters as well unnecessarily).
+func Escape(in string) string {
+	out := ``
+	for _, r := range in {
+		switch r {
+		case '\t':
+			out += `\t`
+		case '\b':
+			out += `\b`
+		case '\f':
+			out += `\f`
+		case '\n':
+			out += `\n`
+		case '\r':
+			out += `\r`
+		case '\\':
+			out += `\\`
+		case '"':
+			out += `\"`
+		default:
+			out += string(r)
+		}
+	}
+	return out
+}
+
+// Marshal mimics json.Marshal from the encoding/json package without
+// the broken, unnecessary HTML escapes and extraneous newline that the
+// json.Encoder adds. Call this from your own MarshalJSON methods to get
+// JSON rendering that is more readable and compliant with the JSON
+// specification (unless you are using the extremely rare case of
+// dumping that into HTML, for some reason). Note that this cannot be
+// called from any structs MarshalJSON method on itself because it will
+// cause infinite functional recursion. Write a proper MarshalJSON
+// method or create a dummy struct and call json.Marshal on that
+// instead.
+func Marshal(v any) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.SetEscapeHTML(false)
+	err := enc.Encode(v)
+	return []byte(strings.TrimSpace(buf.String())), err
+}
+
+// MarshalIndent mimics json.Marshal from the encoding/json package but
+// without the escapes, etc. See Marshal.
+func MarshalIndent(v any, a, b string) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent(a, b)
+	err := enc.Encode(v)
+	return []byte(strings.TrimSpace(buf.String())), err
+}
+
+// Unmarshal mimics json.Unmarshal from the encoding/json package.
+func Unmarshal(buf []byte, v any) error {
+	return json.Unmarshal(buf, v)
+}
+
+// This encapsulates anything with the AsJSON interface from this package
+// by simply assigning a new variable with that item as the only value
+// in the structure:
+//
+//	something := []string{"some","thing"}
+//	jsonified := json.This{something}
+//	jsonified.Print()
+type This struct{ This any }
+
+// UnmarshalJSON implements AsJSON
+func (s *This) UnmarshalJSON(buf []byte) error {
+	return json.Unmarshal(buf, &s.This)
+}
+
+// JSON implements AsJSON.
+func (s This) JSON() ([]byte, error) { return json.Marshal(s.This) }
+
+// String implements AsJSON and logs any error.
+func (s This) String() string {
+	byt, err := s.JSON()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(byt)
+}
+
+// Print implements AsJSON printing with fmt.Println (adding a line
+// return).
+func (s This) Print() { fmt.Println(s.String()) }
+
+// Log implements AsJSON.
+func (s This) Log() { log.Print(s.String()) }
