84d17607fa3f212b630487e9e19ffc18635ca41e
Rob Muhlestein <rob@rwx.gg>
2024-10-23 12:23:10 -0400

    fix weekday completer example

--- pkg/core/comp/weekday.go
+++ pkg/core/comp/weekday.go
@@ -8,12 +8,11 @@ import (
 	"github.com/rwxrob/bonzai/pkg/core/fn/filt"
 )
 
-// give own completer for days of the week
 type _ThreeLetterEngWeekday struct{}
 
 var ThreeLetterEngWeekday = new(_ThreeLetterEngWeekday)
 
-func (ThreeLetterEngWeekday) Complete(_ *bonzai.Cmd, args ...string) []string {
+func (_ThreeLetterEngWeekday) Complete(_ *bonzai.Cmd, args ...string) []string {
 	list := []string{"mon", "tue", "wed", "thu", "fri", "sat", "sun"}
 	if len(args) == 0 {
 		return list
--- pkg/core/comp/weekday_test.go
+++ pkg/core/comp/weekday_test.go
@@ -1,1 +1,13 @@
 package comp_test
+
+import (
+	"fmt"
+
+	"github.com/rwxrob/bonzai/pkg/core/comp"
+)
+
+func ExampleThreeLetterEngWeekday() {
+	fmt.Println(comp.ThreeLetterEngWeekday.Complete(nil, `t`))
+	// Output:
+	// [tue thu]
+}
