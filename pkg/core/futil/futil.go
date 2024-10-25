6dce8750ffa23b41fd153361dec5eec221c066b3
Rob Muhlestein <rob@rwx.gg>
2024-10-25 16:33:13 -0400

    doc fixes

--- pkg/core/futil/futil.go
+++ pkg/core/futil/futil.go
@@ -51,8 +51,8 @@ func DupPerms(orig, clone string) error {
 	return os.Chmod(clone, ostats.Mode())
 }
 
-// ModTime returns the FileInfo.ModTime() from Stat() as a convenience
-// or returns a zero time if not. See time.IsZero.
+// ModTime returns the [FileInfo.ModTime] from [Stat] as a convenience
+// or returns a zero time if not. See [time.IsZero].
 func ModTime(path string) time.Time {
 	info, err := os.Stat(path)
 	if err == nil {
@@ -65,7 +65,7 @@ func ModTime(path string) time.Time {
 // Exists returns true if the given path was absolutely found to exist
 // on the system. A false return value means either the file does not
 // exists or it was not able to determine if it exists or not. Use
-// NotExists instead.
+// [NotExists] instead.
 //
 // WARNING: do not use this function if a definitive check for the
 // non-existence of a file is required since the possible indeterminate
