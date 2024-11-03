2e1062fb17422a6949884dc05cc8ba29fa9073d9
Chaitanya Sharma <55058351+Chaitanyabsprip@users.noreply.github.com>
2024-11-03 18:25:45 +0530

    feat(cmds): add sunrise cmd in cmds (#140)

---
+++ cmds/sunrise/cmd.go
@@ -0,0 +1,26 @@
+package sunrise
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/rwxrob/bonzai/comp"
+	bonzai "github.com/rwxrob/bonzai/pkg"
+)
+
+var Cmd = &bonzai.Cmd{
+	Name:  `sunrise`,
+	Short: `showcase all colors of terminal`,
+	Comp:  comp.Cmds,
+
+	MaxArgs: 1,
+
+	Call: func(x *bonzai.Cmd, args ...string) error {
+		var ms int64 = 10
+		if len(args) > 0 {
+			ms, _ = strconv.ParseInt(args[0], 10, 64)
+		}
+		Sunrise(time.Duration(ms) * time.Millisecond)
+		return nil
+	},
+}
---
+++ cmds/sunrise/cmd/sunrise/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rwxrob/bonzai/cmds/sunrise"
+
+func main() {
+	sunrise.Cmd.Run()
+}
