09b6fbcf2768831e98d8c23693a9690a2e5f7b5d
Rob Muhlestein <rob@rwx.gg>
2024-11-05 09:28:03 -0500

    allow dashes, closes #153

--- is/is.go
+++ is/is.go
@@ -1,6 +1,29 @@
 package is
 
+// AllLatinASCIILowerWithDashes checks if the input string [in] contains only
+// lowercase Latin ASCII letters and dashes. It returns false if the string
+// is empty, starts with a dash, or ends with a dash, or if it contains any
+// character outside the range of 'a' to 'z' or the dash character.
+func AllLatinASCIILowerWithDashes(in string) bool {
+	if len(in) == 0 || in[0] == '-' || in[len(in)-1] == '-' {
+		return false
+	}
+	for _, r := range in {
+		if ('a' <= r && r <= 'z') || r == '-' {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+// AllLatinASCIILower checks if the input string [in] contains only lowercase
+// Latin ASCII letters. It returns false if the string is empty or contains
+// any character outside the range of 'a' to 'z'.
 func AllLatinASCIILower(in string) bool {
+	if len(in) == 0 {
+		return false
+	}
 	for _, r := range in {
 		if 'a' <= r && r <= 'z' {
 			continue
@@ -10,7 +33,13 @@ func AllLatinASCIILower(in string) bool {
 	return true
 }
 
+// AllLatinASCIIUpper checks if the input string [in] contains only uppercase
+// Latin ASCII letters. It returns false if the string is empty or contains
+// any character outside the range of 'A' to 'Z'.
 func AllLatinASCIIUpper(in string) bool {
+	if len(in) == 0 {
+		return false
+	}
 	for _, r := range in {
 		if 'A' <= r && r <= 'Z' {
 			continue
