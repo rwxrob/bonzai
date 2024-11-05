ae8e61f1a3dee3debf8bb8fee271dd580584546e
Rob Muhlestein <rob@rwx.gg>
2024-11-04 21:36:38 -0500

    add anim

--- anim/anim.go
+++ anim/anim.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"syscall"
 
-	"github.com/rwxrob/bonzai/term/esc"
-
 	"github.com/rwxrob/bonzai/run"
+	"github.com/rwxrob/bonzai/term/esc"
 )
 
 // SimpleAnimationScreen conveniently sets up an alternate screen buffer
