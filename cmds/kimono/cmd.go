0e8f0c15ea5006634b082d47c1ad4df945dbf8d3
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-06 03:04:51 +0530

    feat(kimono): add alias for cmds

--- cmds/kimono/cmd.go
+++ cmds/kimono/cmd.go
@@ -9,9 +9,10 @@ import (
 )
 
 var Cmd = &bonzai.Cmd{
-	Name: `kimono`,
-	Comp: comp.Cmds,
-	Cmds: []*bonzai.Cmd{sanitizeCmd, workCmd, tagCmd},
+	Name:  `kimono`,
+	Alias: `kmono|km`,
+	Comp:  comp.Cmds,
+	Cmds:  []*bonzai.Cmd{sanitizeCmd, workCmd, tagCmd},
 }
 
 var sanitizeCmd = &bonzai.Cmd{
@@ -24,13 +25,14 @@ var sanitizeCmd = &bonzai.Cmd{
 
 var workCmd = &bonzai.Cmd{
 	Name:      `work`,
+	Alias:     `w`,
 	Comp:      comp.CmdsOpts,
 	Opts:      `on|off`,
 	MinArgs:   1,
 	MaxArgs:   1,
 	MatchArgs: `on|off`,
 	Call: func(x *bonzai.Cmd, args ...string) error {
-		if args[0] == "on" {
+		if args[0] == `on` {
 			return WorkOn()
 		}
 		return WorkOff()
@@ -38,30 +40,32 @@ var workCmd = &bonzai.Cmd{
 }
 
 var tagCmd = &bonzai.Cmd{
-	Name: `tag`,
-	Comp: comp.Cmds,
-	Cmds: []*bonzai.Cmd{tagListCmd, tagBumpCmd},
-	Def:  tagListCmd,
+	Name:  `tag`,
+	Alias: `t`,
+	Comp:  comp.Cmds,
+	Cmds:  []*bonzai.Cmd{tagListCmd, tagBumpCmd},
+	Def:   tagListCmd,
 }
 
 var tagBumpCmd = &bonzai.Cmd{
 	Name:    `bump`,
+	Alias:   `b|up|i|inc`,
 	Comp:    comp.CmdsOpts,
 	Cmds:    []*bonzai.Cmd{tagListCmd},
 	Opts:    `major|minor|patch|m|M|p`,
 	MaxArgs: 1,
 	Call: func(x *bonzai.Cmd, args ...string) error {
-		mustPush := len(os.Getenv("KIMONO_TAG_PUSH")) > 0
+		mustPush := len(os.Getenv(`KIMONO_TAG_PUSH`)) > 0
 		var part VerPart
 		if len(args) == 0 {
 			part = Minor
 		} else {
 			switch args[0] {
-			case "major", "M":
+			case `major`, `M`:
 				part = Major
-			case "minor", "m":
+			case `minor`, `m`:
 				part = Minor
-			case "patch", "p":
+			case `patch`, `p`:
 				part = Patch
 			}
 		}
@@ -71,7 +75,9 @@ var tagBumpCmd = &bonzai.Cmd{
 }
 
 var tagListCmd = &bonzai.Cmd{
-	Name: `list`,
+	Name:  `list`,
+	Alias: `l`,
+	Comp:  comp.Cmds,
 	Call: func(x *bonzai.Cmd, args ...string) error {
 		each.Println(TagList())
 		return nil
