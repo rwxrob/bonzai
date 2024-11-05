d17e6c9edd2e2b72f543a66cc2fce61e67028d89
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-06 03:29:38 +0530

    feat(kimono): allow panic if DEBUG envvar set

--- cmds/kimono/cmd/kimono/main.go
+++ cmds/kimono/cmd/kimono/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/rwxrob/bonzai/cmds/kimono"
+import (
+	"os"
+
+	"github.com/rwxrob/bonzai/run"
+
+	"github.com/rwxrob/bonzai/cmds/kimono"
+)
 
 func main() {
+	if len(os.Getenv(`DEBUG`)) > 0 {
+		run.AllowPanic = true
+	}
 	kimono.Cmd.Run()
 }
