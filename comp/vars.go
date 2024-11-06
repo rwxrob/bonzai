fd6184f6a29da93dc50e45b53cc55eb4f28c09c1
Rob Muhlestein <rob@rwx.gg>
2024-11-06 17:15:24 -0500

    move to new vars.Data

--- comp/vars.go
+++ comp/vars.go
@@ -2,6 +2,7 @@ package comp
 
 import (
 	"github.com/rwxrob/bonzai"
+	"github.com/rwxrob/bonzai/vars"
 )
 
 type _Vars struct{}
@@ -10,9 +11,9 @@ var Vars = new(_Vars)
 
 // Complete takes a [*bonzai.Cmd] and then calls
 func (_Vars) Complete(_ bonzai.Cmd, args ...string) (list []string) {
-	if bonzai.Vars == nil || len(args) == 0 {
+	if vars.Data == nil || len(args) == 0 {
 		return
 	}
-	list, _ = bonzai.Vars.KeysWithPrefix(args[0])
+	list, _ = vars.Data.KeysWithPrefix(args[0])
 	return
 }
