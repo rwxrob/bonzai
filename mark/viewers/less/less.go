58bf8556482039f38292a081708988d06aa57b6a
Rob Muhlestein <rob@rwx.gg>
2024-11-07 11:32:03 -0500

    actually commit less before tagging

---
+++ mark/viewers/less/less.go
@@ -0,0 +1,23 @@
+package less
+
+import (
+	"io"
+
+	"github.com/rwxrob/bonzai/mark"
+	"github.com/rwxrob/bonzai/term"
+)
+
+type viewer struct {
+}
+
+var _ mark.Viewer = new(viewer)
+
+func (v *viewer) View(in io.Reader, r mark.Renderer) error {
+	// TODO detect less pager and use it or just print it.
+	out, err := io.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	term.Print(out)
+	return nil
+}
