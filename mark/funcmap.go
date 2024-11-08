276bbf143d2ba239abacff69150ec1d5e9f3273f
Rob Muhlestein <rob@rwx.gg>
2024-11-08 03:31:13 -0500

    update mark.FuncMap

---
+++ mark/funcmap.go
@@ -0,0 +1,28 @@
+package mark
+
+import (
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"github.com/rwxrob/bonzai/futil"
+	"github.com/rwxrob/bonzai/run"
+)
+
+// FuncMap contains the package the BonzaiMark standard template tag
+// functions. Any [Renderer] must support them all (even if some may
+// output an empty string).
+var FuncMap = template.FuncMap{
+	"exepath":      run.Executable,
+	"exename":      run.ExeName,
+	"execachedir":  run.ExeCacheDir,
+	"exestatedir":  run.ExeStateDir,
+	"execonfigdir": run.ExeConfigDir,
+	"cachedir":     futil.UserCacheDir,
+	"confdir":      futil.UserConfigDir,
+	"homedir":      futil.UserHomeDir,
+	"statedir":     futil.UserStateDir,
+	"pathsep":      func() string { return string(os.PathSeparator) },
+	"pathjoin":     filepath.Join,
+	// TODO add dummy formatting tags
+}
