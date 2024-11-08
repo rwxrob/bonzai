147479d4128e9eba007954093e0f95449035ae4d
Rob Muhlestein <rob@rwx.gg>
2024-11-08 13:17:34 -0500

    rearranged Renderer args

--- mark/mark.go
+++ mark/mark.go
@@ -9,13 +9,18 @@ import (
 // digital data whether it be text, HTML, PDF, or other binary data.
 //
 // To maximize compatibility between Renderers, implementations must
-// only allow input that complies with the current BonzaiMark specification
-// documented in [mark] package.
+// only allow input that complies with the current BonzaiMark
+// specification documented in [mark] package. Implementations may
+// extend that specification and support more complex markups but
+// developers must understand such specialization will be much less
+// useful to as many people.
 //
 // See the following for examples of implementations:
 //
 //   - [pkg/github.com/rwxrob/bonzai/mark/renderers]
 //   - [pkg/github.com/rwxrob/bonzai/cmds/help]
 type Renderer interface {
-	Render(this any, zmark io.Reader, m *template.FuncMap) (io.Reader, error)
+	Render(this any, m *template.FuncMap, zmark io.Reader) (io.Reader, error)
 }
+
+// man.Render(x, x.Mark(), mark.FuncMap)
