fe38114b582e40656543dcd5aef3cfefdd9e3392
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-09 14:24:26 +0530

    fix(comp): re-enable tests and fix bug in Cmds

--- comp/cmds.go
+++ comp/cmds.go
@@ -7,22 +7,22 @@ import (
 
 type _Cmds struct{}
 
+// Cmds is a [bonzai.Completer] that returns all non-hidden
+// [bonzai.Cmd.Cmds]
 var Cmds = new(_Cmds)
 
 // Complete returns all visible [Cmd.Cmds] that match [futil.HasPrefix]
 // for arg[0] . See [bonzai.Completer].
 func (_Cmds) Complete(x bonzai.Cmd, args ...string) []string {
 	list := []string{}
+	if len(args) == 0 {
+		return list
+	}
 	for _, c := range x.Cmds {
 		if c.IsHidden() {
 			continue
 		}
 		list = append(list, c.Name)
 	}
-
-	if len(args) == 0 {
-		return list
-	}
-
 	return filt.HasPrefix(list, args[0])
 }
--- comp/cmds_test.go
+++ comp/cmds_test.go
@@ -4,36 +4,38 @@
 package comp_test
 
 import (
+	"fmt"
+
 	"github.com/rwxrob/bonzai"
+
+	"github.com/rwxrob/bonzai/comp"
 )
 
 func ExampleCmds_Complete() {
-	foo := new(bonzai.Cmd)
+	foo := *new(bonzai.Cmd)
 	foo.Opts = `box`
 	foo.Add(`bar`)
 	foo.Add(`blah`)
-	/*
-		// if no args, we have to assume the command isn't finished yet
-		fmt.Println(comp.Cmds.Complete(foo))
+	// if no args, we have to assume the command isn't finished yet
+	fmt.Println(comp.Cmds.Complete(foo))
 
-		// we know it's not a command, but no prefix just yet
-		// (usually this is when a space has been added after the command)
-		fmt.Println(comp.Cmds.Complete(foo, ""))
+	// we know it's not a command, but no prefix just yet
+	// (usually this is when a space has been added after the command)
+	fmt.Println(comp.Cmds.Complete(foo, ""))
 
-		// everything that begins with a (nothing)
-		fmt.Println(comp.Cmds.Complete(foo, `a`))
+	// everything that begins with a (nothing)
+	fmt.Println(comp.Cmds.Complete(foo, `a`))
 
-		// everything that begins with b (which is everything)
-		fmt.Println(comp.Cmds.Complete(foo, `b`))
+	// everything that begins with b (which is everything)
+	fmt.Println(comp.Cmds.Complete(foo, `b`))
 
-		// everything that begins with bl (just blah)
-		fmt.Println(comp.Cmds.Complete(foo, `bl`))
-	*/
-	//Output:
+	// everything that begins with bl (just blah)
+	fmt.Println(comp.Cmds.Complete(foo, `bl`))
+
+	// Output:
 	// []
 	// [bar blah]
 	// []
 	// [bar blah]
 	// [blah]
-
 }
