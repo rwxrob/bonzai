9e99b38c7fd8ac86e3638313a83b34ca8b5314a8
Chaitanya Sharma <55058351+Chaitanyabsprip@users.noreply.github.com>
2024-11-11 18:31:37 +0530

    feat(bonzai): debug envvar should allow panic (#196)

--- cmds/kimono/cmd/kimono/main.go
+++ cmds/kimono/cmd/kimono/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/rwxrob/bonzai/run"
-
 	"github.com/rwxrob/bonzai/cmds/kimono"
 )
 
 func main() {
-	if len(os.Getenv(`DEBUG`)) > 0 {
-		run.AllowPanic = true
-	}
 	kimono.Cmd.Run()
 }
