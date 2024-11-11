34360aaf2fa5bf1434d6d77f7791d350a7cd9925
Rob Muhlestein <rob@rwx.gg>
2024-11-11 10:51:49 -0500

    add fn/doc.go

---
+++ fn/doc.go
@@ -0,0 +1,4 @@
+/*
+Package fn standardizes functional types for use in Bonzai projects so that type switching on them can be done consistently. It also provides a collection of common type implementations.
+*/
+package fn
