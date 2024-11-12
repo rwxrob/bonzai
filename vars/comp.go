660c1c8d383a2ace9f8d7174408805c2f6c6054c
Chaitanya Sharma <55058351+Chaitanyabsprip@users.noreply.github.com>
2024-11-12 21:42:34 +0530

    fix(vars): update Comp impl for revised interface (#201)

--- vars/comp.go
+++ vars/comp.go
@@ -1,15 +1,11 @@
 package vars
 
-import (
-	"github.com/rwxrob/bonzai"
-)
-
 type _Comp struct{}
 
 var Comp = new(_Comp)
 
 // Complete takes a [*bonzai.Cmd] and then calls
-func (_Comp) Complete(_ bonzai.Cmd, args ...string) (list []string) {
+func (_Comp) Complete(args ...string) (list []string) {
 	if Data == nil || len(args) == 0 {
 		return
 	}
