e5fa15b26c36c32d4704a501a613e0dbf77c45c2
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-11-12 20:02:48 +0530

    fix(vars): update Comp impl for revised interface

--- vars/comp_test.go
+++ vars/comp_test.go
@@ -4,23 +4,19 @@
 package vars_test
 
 import (
-	"github.com/rwxrob/bonzai"
 	"github.com/rwxrob/bonzai/json"
 
 	"github.com/rwxrob/bonzai/vars"
 )
 
 func ExampleComp_Complete() {
-	foo := bonzai.Cmd{}
-	foo.Comp = vars.Comp
-
 	defer vars.Data.Delete(`some`)
 	defer vars.Data.Delete(`someother`)
 	vars.Data.Set(`some`, `thing`)
 	vars.Data.Set(`someother`, `awesome`)
 
-	json.This{This: vars.Comp.Complete(foo, `s`)}.Print()
-	json.This{This: vars.Comp.Complete(foo, `some`)}.Print()
+	json.This{This: vars.Comp.Complete(`s`)}.Print()
+	json.This{This: vars.Comp.Complete(`some`)}.Print()
 
 	// Output:
 	// ["some","someother"]
