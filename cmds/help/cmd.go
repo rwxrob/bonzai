dd37441bd5e1110efb553f78cbd3e50784becf39
Rob Muhlestein <rob@rwx.gg>
2024-11-14 21:23:49 -0500

    update deps, add aka

--- cmds/help/cmd.go
+++ cmds/help/cmd.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/rwxrob/bonzai"
 	"github.com/rwxrob/bonzai/mark"
+	"github.com/rwxrob/bonzai/mark/funcs"
 	"github.com/rwxrob/bonzai/term"
+	"github.com/rwxrob/bonzai/to"
 )
 
 var Renderer mark.Renderer
@@ -15,22 +17,26 @@ var Cmd = &bonzai.Cmd{
 	Vers:  `v0.4.1`,
 	Short: `display command help`,
 	Alias: `-h|--help|--h|/?`,
-	Long: ` 
-		The {{.Name}} command displays the help information for the
+	Long: `
+		The {{aka .}} command displays the help information for the
 		immediate previous command unless it is passed arguments, in which
 		case it resolves the arguments as if they were passed to the
 		previous command and the help for the leaf command is displayed
 		instead.`,
 
 	Do: func(x *bonzai.Cmd, args ...string) error {
+		funcs := funcs.Map
+
 		if len(args) > 0 {
 			x, args = x.Caller().Seek(args)
 		} else {
 			x = x.Caller()
 		}
 
+		funcs = to.MergedMaps(funcs, x.Funcs)
+
 		if Renderer != nil {
-			r, err := Renderer.Render(x, x.Funcs, x.Mark())
+			r, err := Renderer.Render(x, funcs, x.Mark())
 			if err != nil {
 				return err
 			}
@@ -42,6 +48,7 @@ var Cmd = &bonzai.Cmd{
 			return nil
 		}
 
+		x.Funcs = funcs
 		term.Print(x.MarkString())
 
 		return nil
