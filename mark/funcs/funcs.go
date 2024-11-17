c84f07ee60f522c77bc61cb88249f43e98c5070f
Rob Muhlestein <rob@rwx.gg>
2024-11-17 02:13:15 -0500

    230 change markrender to fill (#247)
    * move cmdtree to funcs package, allow override

--- mark/funcs/funcs.go
+++ mark/funcs/funcs.go
@@ -10,6 +10,7 @@ import (
 	"github.com/rwxrob/bonzai"
 	"github.com/rwxrob/bonzai/futil"
 	"github.com/rwxrob/bonzai/run"
+	"github.com/rwxrob/to"
 )
 
 var Map = template.FuncMap{
@@ -26,6 +27,7 @@ var Map = template.FuncMap{
 	"pathjoin":     filepath.Join,
 	"aka":          AKA,
 	"code":         Code,
+	"cmdtree":      CmdTree,
 }
 
 // AKA returns the name followed by all aliases in parenthesis joined
@@ -57,3 +59,74 @@ func AKA(x *bonzai.Cmd) string {
 // [bonzai.Cmd].Short but cannot because backticks are already used to
 // contain the multi-line text itself.
 func Code(it any) string { return fmt.Sprintf("`%v`", it) }
+
+// CmdTree generates and returns a formatted string representation
+// of the command tree for the [Cmd] instance and all its [Cmd].Cmds
+// subcommands. It aligns [Cmd].Short summaries in the output for better
+// readability, adjusting spaces based on the position of the dashes.
+func CmdTree(x *bonzai.Cmd) string {
+	tree := cmdTree(x, 2)
+	lines := to.Lines(tree)
+	var widest int
+	for _, line := range lines {
+		if length := countRunes(line, '←'); length > widest {
+			widest = length
+		}
+	}
+	for i, line := range lines {
+		parts := strings.Split(line, "←")
+		if len(parts) > 1 {
+			lines[i] = fmt.Sprintf("    %-*v←%v", widest, parts[0], parts[1])
+		} else {
+			lines[i] = "    " + line
+		}
+	}
+	return strings.Join(lines, "\n") + "\n"
+}
+
+func cmdTree(x *bonzai.Cmd, depth int) string {
+	out := new(strings.Builder)
+	addbranch := func(level int, c *bonzai.Cmd) error {
+		if level > depth {
+			return nil
+		}
+		for range level {
+			out.WriteString(`  `)
+		}
+		if c.IsHidden() {
+			out.WriteString("(hidden) ← contains hidden subcommands\n")
+			return nil
+		}
+		name := c.Name
+		if len(name) == 0 {
+			name = `noname`
+		}
+		out.WriteString(name)
+		if len(c.Short) > 0 {
+			out.WriteString(" ← " + c.Short)
+		}
+		caller := c.Caller()
+		if caller != nil && caller.Def == c {
+			if len(c.Short) == 0 {
+				out.WriteString(" ←")
+			}
+			out.WriteString(" (default)")
+		}
+		out.WriteString("\n")
+		return nil
+	}
+	x.WalkDeep(addbranch, nil)
+	return out.String()
+}
+
+// strings.Index and IndexRune don't do what you think
+func countRunes(in string, it rune) int {
+	var i int
+	for _, r := range []rune(in) {
+		if r == it {
+			return i
+		}
+		i++
+	}
+	return i
+}
